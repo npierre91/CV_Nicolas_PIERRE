f9cf0bd327a91600463271c7cf5fe602b83650d2
Forth <33458877+Forthtilliath@users.noreply.github.com>
2021-06-15 19:32:28 +0200

    Removed pdfgenerator

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import Skills from "./components/Skills";
 import Profil from "./components/Profil";
 import FormationsExperiences from "./components/FormationsExperiences";
 import DarkMode from "./components/DarkMode";
-import { Preview, print } from "react-html2pdf";
-import PictureAsPdfIcon from "@material-ui/icons/PictureAsPdf";
+// import { Preview, print } from "react-html2pdf";
+// import PictureAsPdfIcon from "@material-ui/icons/PictureAsPdf";
 
 function App() {
-  const handleGenerateCv = () => {
-    let cvTemplate = document.querySelector("#cv-print");
-    cvTemplate.setAttribute("style", { width: "210mm !important" });
-    cvTemplate.classList.add("cv-print");
-    document.body.classList.remove("dark");
-    setTimeout(() => {
-      print("cv", "cv-print");
-      cvTemplate.setAttribute("style", { width: "100% !important" });
-      cvTemplate.classList.remove("cv-print");
-    }, 300);
-  };
+  // const handleGenerateCv = () => {
+  //   let cvTemplate = document.querySelector("#cv-print");
+  //   cvTemplate.setAttribute("style", { width: "210mm !important" });
+  //   cvTemplate.classList.add("cv-print");
+  //   document.body.classList.remove("dark");
+  //   setTimeout(() => {
+  //     print("cv", "cv-print");
+  //     cvTemplate.setAttribute("style", { width: "100% !important" });
+  //     cvTemplate.classList.remove("cv-print");
+  //   }, 300);
+  // };
   return (
     // <Preview id={"cv-print"}>
       <div className="App">
@@ -26,7 +26,7 @@ function App() {
           <div className="sidebar">
             <div className="actions">
               <DarkMode />
-              <button onClick={handleGenerateCv}><PictureAsPdfIcon /></button>
+              {/* <button onClick={handleGenerateCv}><PictureAsPdfIcon /></button> */}
             </div>
             <User />
             <Skills />
