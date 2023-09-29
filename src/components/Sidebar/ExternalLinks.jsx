43f3cfc5b2ced4e6fd8d973a69570d3979899b6e
Nicolas PIERRE <npierre9@yahoo.fr>
2023-09-29 19:54:43 +0200

    feat(prettier-external-links): Makes the external links prettier

--- src/components/Sidebar/ExternalLinks.jsx
+++ src/components/Sidebar/ExternalLinks.jsx
@@ -3,19 +3,20 @@ import { Link } from 'react-router-dom'
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import { useTranslation } from '../../lib/intl';
+import "./ExternalLinks.css"
 
 const ExternalLinks = () => {
     const { t } = useTranslation()
     return (
-        <div className="external_Links" >
+        <div className="external__Links" >
             <h2 className="h2">{t("EXTERNAL_LINK_TITLE")}</h2>
-            <div className='LinkedIn'>
-                <LinkedInIcon style={{ fontSize: 20 }} />
+            <div className='external__link'>
+                <LinkedInIcon style={{ fontSize: 25 }} />
                 <Link to="https://www.linkedin.com/in/nicolas-pierre-6387a0222/" style={{ textDecoration: 'underline' }}>LinkedIn</Link>
             </div>
             <br></br>
-            <div className='github'>
-                <GitHubIcon style={{ fontSize: 20 }} />
+            <div className='external__link'>
+                <GitHubIcon style={{ fontSize: 25 }} />
                 <Link to="https://github.com/npierre91" style={{ textDecoration: 'underline' }}>GitHub</Link>
             </div>
             <br></br>
