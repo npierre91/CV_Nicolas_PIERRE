f2ad384422eef8e7ad98642f3bec9be05fbe936b
Nicolas PIERRE <npierre9@yahoo.fr>
2023-10-01 14:03:40 +0200

    fix(display): fixed experiences display order and circle size

--- src/datas/Experiences.js
+++ src/datas/Experiences.js
@@ -1,14 +1,6 @@
 const experiencesData = {
   fr: [
     {
-      title: "Stage d'apprenti ingénieur - Recherche en data science",
-      date: "01/06/2021 - 04/09/2021",
-      location: "Stanford University, California",
-      text: "Dans le cadre d'un stage de 2ème année, j'ai eu l'occasion de développer un modèle d'intelligence artificielle pour reconnaître des actions réalisées dans des vidéos. Ce modèle a été développé pour un challenge de l'ICCV 2021 et est orienté dans le domaine de la médecine.",
-      technos: ["Python 3, Tensorflow", "Linux environment"],
-      missions: ["Développement d'un réseau de neurones pouvant reconnaître des actions particulières dans une vidéo", "Pré-traitement des données pour les rendre compatibles avec le modèle d'IA développé"]
-    },
-    {
       title: "Ingénieur développeur Web - Full Stack",
       date: "04/04/2022 - 12/01/2024",
       location: "Goshaba, Paris",
@@ -16,15 +8,16 @@ const experiencesData = {
       technos: ["Javascript, Typescript, Graphql, NestJS, React, NextJS", "MongoDB", "Postman, Jest, Storybook, Cypress, NewRelic", "Gitlab et Gitlab CI, AWS (CloudWatch, SecretsManager, S3, Inspector), Serverless"],
       missions: ["Développement et maintien d'une API externe pour se connecter à des ATS", "Développement de fonctionnalités diverses en backend et en frontend sur le produit Goshaba", "Développement de tests unitaires et E2E via Postman, Jest et Cypress"],
     },
-  ], en: [
     {
-      title: "Engineering internship - Data science research",
+      title: "Stage d'apprenti ingénieur - Recherche en data science",
       date: "01/06/2021 - 04/09/2021",
       location: "Stanford University, California",
-      text: "As part of a 2nd year internship, I had the opportunity to develop an artificial intelligence model for recognising actions performed in videos. This model was developed for an ICCV 2021 challenge and is oriented towards the field of medicine.",
+      text: "Dans le cadre d'un stage de 2ème année, j'ai eu l'occasion de développer un modèle d'intelligence artificielle pour reconnaître des actions réalisées dans des vidéos. Ce modèle a été développé pour un challenge de l'ICCV 2021 et est orienté dans le domaine de la médecine.",
       technos: ["Python 3, Tensorflow", "Linux environment"],
-      missions: ["Development of a neural network capable of recognising specific actions in a video", "Pre-processing of the data to make it compatible with the AI model developed"]
+      missions: ["Développement d'un réseau de neurones pouvant reconnaître des actions particulières dans une vidéo", "Pré-traitement des données pour les rendre compatibles avec le modèle d'IA développé"]
     },
+
+  ], en: [
     {
       title: "Web Developer Engineer - Full Stack",
       date: "04/04/2022 - 12/01/2024",
@@ -33,6 +26,15 @@ const experiencesData = {
       technos: ["Javascript, Typescript, Graphql, NestJS, React, NextJS", "MongoDB", "Postman, Jest, Storybook, Cypress, NewRelic", "Gitlab and Gitlab CI, AWS (CloudWatch, SecretsManager, S3, Inspector), Serverless"],
       missions: ["Development and maintenance of an external API to connect to ATS", "Development of various backend and frontend functionalities for the Goshaba product", "Development of unit and E2E tests using Postman, Jest and Cypress"],
     },
+    {
+      title: "Engineering internship - Data science research",
+      date: "01/06/2021 - 04/09/2021",
+      location: "Stanford University, California",
+      text: "As part of a 2nd year internship, I had the opportunity to develop an artificial intelligence model for recognising actions performed in videos. This model was developed for an ICCV 2021 challenge and is oriented towards the field of medicine.",
+      technos: ["Python 3, Tensorflow", "Linux environment"],
+      missions: ["Development of a neural network capable of recognising specific actions in a video", "Pre-processing of the data to make it compatible with the AI model developed"]
+    },
+
   ]
 };
 export default experiencesData;
