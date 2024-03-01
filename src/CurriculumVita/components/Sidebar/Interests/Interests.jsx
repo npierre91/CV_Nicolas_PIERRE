233fafdb3242bbf15609bba8b01ae845e323befd
Nicolas PIERRE <npierre9@yahoo.fr>
2024-03-01 14:12:01 +0100

    fixed bug on collapse displays in CV, and removed useless parts of code

--- src/CurriculumVita/components/Sidebar/Interests/Interests.jsx
+++ src/CurriculumVita/components/Sidebar/Interests/Interests.jsx
@@ -1,7 +1,7 @@
 import { mdiController, mdiChessKnight, mdiFencing, mdiDivingScuba, mdiCamera } from '@mdi/js';
 import Icon from '@mdi/react';
 import { ExpandLess, ExpandMore } from '@mui/icons-material';
-import { Collapse, List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import { Collapse, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
 import React from 'react';
 
 import ChessMe from '../../../datas/ChessProfile';
@@ -33,53 +33,53 @@ const Interests = () => {
     <div className="interests">
       <h2 className="h2">{t("INTEREST_TITLE")}</h2>
       <List disablePadding className='interest__list'>
-        <ListItemButton disableGutters onClick={handleChange1}>
+        <ListItem disableGutters onClick={handleChange1}>
           <ListItemIcon>
             <Icon path={mdiDivingScuba} size={1} />
           </ListItemIcon>
           <ListItemText primary={t("INTEREST_1")} />
           {openInterest1 ? <ExpandLess /> : <ExpandMore />}
-        </ListItemButton>
+        </ListItem>
         <Collapse in={openInterest1}>
           <p>{t("INTEREST_1_TEXT")}</p>
         </Collapse>
-        <ListItemButton disableGutters onClick={handleChange2}>
+        <ListItem disableGutters onClick={handleChange2}>
           <ListItemIcon>
             <Icon path={mdiFencing} size={1} />
           </ListItemIcon>
           <ListItemText primary={t("INTEREST_2")} />
           {openInterest2 ? <ExpandLess /> : <ExpandMore />}
-        </ListItemButton>
+        </ListItem>
         <Collapse in={openInterest2}>
           <p>{t("INTEREST_2_TEXT")}</p>
         </Collapse>
-        <ListItemButton disableGutters onClick={handleChange3}>
+        <ListItem disableGutters onClick={handleChange3}>
           <ListItemIcon>
             <Icon path={mdiChessKnight} size={1} />
           </ListItemIcon>
           <ListItemText primary={t("INTEREST_3")} />
           {openInterest3 ? <ExpandLess /> : <ExpandMore />}
-        </ListItemButton>
+        </ListItem>
         <Collapse in={openInterest3}>
           <p>{t("INTEREST_3_TEXT", { blitzElo: blitzElo, rapidElo: rapidElo })}</p>
         </Collapse>
-        <ListItemButton disableGutters onClick={handleChange4}>
+        <ListItem disableGutters onClick={handleChange4}>
           <ListItemIcon>
             <Icon path={mdiCamera} size={1} />
           </ListItemIcon>
           <ListItemText primary={t("INTEREST_4")} />
           {openInterest4 ? <ExpandLess /> : <ExpandMore />}
-        </ListItemButton>
+        </ListItem>
         <Collapse in={openInterest4}>
           <p>{t("INTEREST_4_TEXT")}</p>
         </Collapse>
-        <ListItemButton disableGutters onClick={handleChange5}>
+        <ListItem disableGutters onClick={handleChange5}>
           <ListItemIcon>
             <Icon path={mdiController} size={1} />
           </ListItemIcon>
           <ListItemText primary={t("INTEREST_5")} />
           {openInterest5 ? <ExpandLess /> : <ExpandMore />}
-        </ListItemButton>
+        </ListItem>
         <Collapse in={openInterest5}>
           <p>{t("INTEREST_5_TEXT")}</p>
         </Collapse>
