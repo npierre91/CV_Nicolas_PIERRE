4c230d8c822f7aab81c387b1e7eb1c74c8b9f4f6
Nicolas PIERRE <npierre9@yahoo.fr>
2024-03-05 18:25:44 +0100

    added scroll pager and second page as profile

--- src/GlobalMenu/index.jsx
+++ src/GlobalMenu/index.jsx
@@ -1,18 +1,26 @@
+import React from 'react';
+import ReactPageScroller from 'react-page-scroller';
+
 import LandingPage from "./LandingPage/landingPage";
 import NavBar from "./NavBar/navBar"
-
 import "./index.css";
+import SecondPage from './SecondPage/secondePage';
 
 
 const MenuPage = () => {
+    const [currentPage, setCurrentPage] = React.useState(null)
+    const handlePageChange = (pageNumber) => {
+        setCurrentPage(pageNumber)
+    }
     return (
         <div className="main-menu-grid__container">
             <div className="main-menu-sidebar">
                 <NavBar />
             </div>
-            <div className="main-menu-main-page">
+            <ReactPageScroller pageOnChange={handlePageChange}>
                 <LandingPage />
-            </div>
+                <SecondPage />
+            </ReactPageScroller>
         </div>
     )
 }
