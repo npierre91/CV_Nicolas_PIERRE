a307447f7ef73107eeaeeb7f4fdebda45785c9f1
Nicolas PIERRE <npierre9@yahoo.fr>
2024-03-05 18:25:44 +0100

    added scroll pager and second page as profile

--- src/GlobalMenu/SecondPage/secondePage.jsx
+++ src/GlobalMenu/SecondPage/secondePage.jsx
@@ -1,14 +1,11 @@
-
-import TypewriterComponent from "typewriter-effect"
-
-import "./landingPage.css"
+import TypewriterComponent from "typewriter-effect";
 
 const strings = [
-    "<font color='green'>Potentiel</font> et <font color='#75151E'>compétences</font> à votre service",
-    "<font color='green'>Potential</font> and <font color='#75151E'>expertise</font> at your service",
+    "Un profil <font color='#75151E'>passionné</font>, <font color='#DFC779'>volontaire</font> et <font color='green'>ambitieux</font>",
+    "A <font color='#75151E'>driven</font>, <font color='#DFC779'>determined</font> and <font color='green'>ambitious</font> profile"
 ]
 
-const LandingPage = () => {
+const SecondPage = () => {
     return (
         <div className="main-page-typewriter">
             <TypewriterComponent
@@ -23,7 +20,7 @@ const LandingPage = () => {
                 }}
             />
         </div>
-    )
-}
+    );
+};
 
-export default LandingPage
+export default SecondPage
