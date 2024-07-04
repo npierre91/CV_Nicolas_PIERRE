c59fe2638e34dc372615863d15e3d338f02242fe
Nicolas PIERRE <npierre9@yahoo.fr>
2024-07-04 17:46:05 +0200

    update experiences to add solutec

--- src/CurriculumVita/components/Main/Experiences.jsx
+++ src/CurriculumVita/components/Main/Experiences.jsx
@@ -7,7 +7,7 @@ import { useTranslation } from "../../lib/intl";
 
 const Experiences = ({ datas }) => {
   const { t } = useTranslation()
-  const initialStateOpen = [false, false]
+  const initialStateOpen = [false, false, false]
   const [open, setOpen] = React.useState(initialStateOpen)
   const handleChange = (key) => {
     const newState = open.map((state, index) => {
