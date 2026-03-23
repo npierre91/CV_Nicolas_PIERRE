c7957c4a9a96275aa1eede705c84bcbecf35fadd
Nicolas PIERRE <npierre9@yahoo.fr>
2026-03-24 00:41:10 +0100

    removed unused comment

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import CurriculumVita from "./CurriculumVita";
 import IntlProvider from "./CurriculumVita/context/IntlProvider";
 import PhotoPortfolio from "./PhotoPortfolio";
-// import { Preview, print } from "react-html2pdf";
+
 // import PictureAsPdfIcon from "@material-ui/icons/PictureAsPdf";
 
 const theme = createTheme()
